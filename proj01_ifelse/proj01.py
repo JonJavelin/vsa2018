f042a1b1a240ff7a5eb3c37b11f2f1d6262de66f
JonJavelin <39975345+JonJavelin@users.noreply.github.com>
2018-06-12 09:50:59 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -18,9 +18,65 @@ print "You will graduate from High_school in" + str(graduate)
 print "Hello"
 name = raw_input("What is your name?")
 month = int(raw_input("What month were you born in?"))
-day = int(raw_input[("What day in ") + str(month) ("were you born in?")])
+month = month[0].upper() + month[1:].lower()
+day = int(raw_input[("What day in ") + month ("were you born in?")])
 Month = raw_input("What month is it now?")
-Day = raw_input("What day in") + str(Month) ("is it right now?")
+Month = Month[0].upper() + Month[1:].lower
+Day = raw_input("What day in") + Month ("is it right now?")
 mOnth = 12+month-Month
+
+dAY = Day - day
+if Month == "January":
+    Month_num = 1
+elif Month == "February":
+    Month_num = 2
+elif Month == "March":
+    Month_num = 3
+elif Month == "April":
+    Month_num = 4
+elif Month == "May":
+    Month_num = 5
+elif Month == "June":
+    Month_num = 6
+elif Month == "July":
+    Month_num = 7
+elif Month == "August":
+    Month_num = 8
+elif Month == "September":
+    Month_num = 9
+elif Month == "October":
+    Month_num = 10
+elif Month == "November":
+    Month_num = 11
+else Month_num = 12
+
+if month == "January":
+    month_num = 1
+elif month == "February":
+    month_num = 2
+elif month == "March":
+    month_num = 3
+elif month == "April":
+    month_num = 4
+elif month == "May":
+    month_num = 5
+elif month == "June":
+    month_num = 6
+elif month == "July":
+    month_num = 7
+elif month == "August":
+    month_num = 8
+elif month == "September":
+    month_num = 9
+elif month == "October":
+    month_num = 10
+elif month == "November":
+    month_num = 11
+else month_num = 12
+moNTH = (month + 11) * 30
+if month == Month and day<Day:
+    moNTH - dAY
+print "Your Birthday is in" +
+
 
 # If you complete extensions, describe your extensions here!
